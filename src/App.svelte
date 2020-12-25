8f7ec48dd4213794c7e474ddf4ea6c355b05c785
zura12337 <zura.kurta@gmail.com>
2020-12-26 02:16:35 +0400

    ✨ Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Books from "./components/Books.svelte";
+	import Navbar from "./components/Navbar.svelte";
+</script>
+
+<style>
+	.container {
+		margin: 50px 200px;
+	}
+</style>
+
+<main>
+	<Navbar />
+	<div class="container">
+		<Books />
+	</div>
+</main>
