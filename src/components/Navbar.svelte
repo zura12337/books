dd39a5836a33f5a7b29e7106eb9aa6ec2f27d66c
zura12337 <zura.kurta@gmail.com>
2020-12-26 02:17:18 +0400

    ➕ Add Navbar Component.

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,31 @@
+<style>
+  .nav-list {
+    display: flex;
+    list-style: none;
+    margin-left: 50px;
+  }
+  .nav-item {
+    margin-left: 10px;
+  }
+  .nav-link {
+    text-decoration: none;
+  }
+  .nav-link:hover {
+    text-decoration: underline;
+  }
+  .navbar {
+    background-color: aliceblue;
+    height: 100px;
+    display: flex;
+    align-items: center;
+  }
+</style>
+
+<main>
+  <div class="navbar">
+    <ul class="nav-list">
+      <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="#">Books</a></li>
+    </ul>
+  </div>
+</main>
