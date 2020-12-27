dad24540fbc89b755a950e624e6e323f036a383b
zura12337 <zura.kurta@gmail.com>
2020-12-27 14:54:11 +0400

    ✏ Change Maxresult.

--- src/services/booksService.js
+++ src/services/booksService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function getBooks() {
   const response = await axios.get(
-    "https://www.googleapis.com/books/v1/volumes?q=a"
+    "https://www.googleapis.com/books/v1/volumes?q=a&maxResults=12"
   );
   return response.data;
 }
